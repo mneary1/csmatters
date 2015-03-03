088f6ce8743e5250ddd1b0f864a4a19dba8e7368
Caroline Kery <ckery1@umbc.edu>
2015-03-03 11:19:29 -0500

    These are exercises for Unit 1

---
+++ Unit1/inputCheckingStudent.py
@@ -0,0 +1,127 @@
+#This file's purpose is to practice checking inputs and using operators such
+#as loops and if statements. 
+
+def main():
+    #Print out welcome
+    print("Hello there! \n This program will take a number and run it through")
+    print(" a series of tests.")
+
+    #Use getValidInput to get a number
+    number = getValidInput("Please enter your number.")
+
+    #This boolean will be false until the user decides to end the program
+    done = False
+ 
+    #This loop will keep going until the user indicates the program should end
+    while not done:
+        
+        #Print out menu
+        print("\nPlease pick a choice:\n1. Is Even?\n2. Is positive?")
+        print("3. Is a factor of 3?\n4.Is a perfect square?\n5. Quit")
+      
+        #Get input from the user
+        choice = getValidInput("")
+        
+        print("")
+ 
+        #These if statements will check the input from the user and run the
+        #correct tests
+        if(choice == 5):
+            #If the user typed 5, they want to quit, so we make our loop end
+            #by making done true
+            done = True;
+        
+        elif(choice == 4):
+            perfectSquare(number)
+       
+        elif(choice == 3):
+            factorOfThree(number)
+
+        elif(choice == 2):
+            isPositive(number)
+        
+        elif(choice == 1):
+            isEven(number)
+   
+        else:
+            #If the number given was not between one and 5, it will hit this
+            #else and print the error.
+            print("Invalid number. Please try again.")
+
+#This function checks if a given input is a number. It will keep asking
+#until the user types in a number.
+def getValidInput(message):
+    
+    good = False
+    #This is very similiar to the while loop above. It will keep going until
+    #the boolean good is proven true. This happens when a valid input is 
+    #entered
+
+    while ######CODE:
+        #Get input from user. Check if it is a number. 
+        number = input(message)
+
+        if(number.isdigit() == True):
+            #If it is, end the loop
+            good = True;
+        
+        else:
+            #if it is not a number, tell the user
+            print("That is not a number. Please try again.")
+    #return the number
+    return int(number)
+
+
+
+#This function checks if the number is even
+def isEven (number):
+
+    #What makes a number even? Fill in the contents of this if statement
+    if(##################CODE):
+        print("It is even")
+    else:
+        print("It is odd")
+    return
+
+#This function checks if the number is positive
+def isPositive(number):
+   
+    #What nmakes a number positive? Fill in the if statement
+    if(#########CODE):
+        print("It is positive")
+    else:
+        print("It is negative")
+    return
+
+#This function checks if the number is a multiple of three
+def factorOfThree(number):
+   
+    #This if statement should look a lot like the one in isEven
+    if(#######CODE):
+        print("It is divisible by three")
+    else:
+        print("It is not divisible by three")
+
+#This function checks if the number is a perfect square
+def perfectSquare(number):
+
+    #This variable keeps track of the root we are checking
+    root = 1
+
+    #How would we check if a number is a perfect square? Use root to fill
+    #in the condition for the while loop and if statement
+    while(######CODE):
+
+        
+        if(############CODE):
+            print("It is a perfect square")
+            return
+
+        #This makes the value of root increase by one
+        root+=1
+
+    print("It is not a perfect square")
+    return 
+
+main() 
+    
---
+++ Unit1/orderOfOperations.py
@@ -0,0 +1,58 @@
+#This file contains a simple program that computes mathematical operations
+
+#Students should predict the answers to each operation, then uncomment
+#the print statements to find out the computed values.
+
+#Next students should add parentheses to get these values:
+#a)37
+#b)10
+#c)-18
+#d)-2
+#NOTE: the ** symbol is the Python version of the power sign so 2**3 = 8
+a = 2 * 14 + 8 * 3 - 13 * 3
+
+#print("The answer to a is :",a)
+
+b = 5 % 3 + 2 ** 3 + 5
+
+#print("The answer to b is :",b)
+
+c = 3 - 7 * 5 + 6 / 3 
+
+#print("The answer to c is :",c)
+
+d = 2 ** 4 / 8 * 5 - 6
+
+#print("The answer to d is :",d)
+
+
+#Integer division (denoted as // ) is different from normal division
+#Looking at these examples, predict what the difference might be.
+
+e = 7/2
+f = 7//2
+#print("e and f are:",e," and :",f)
+
+#Now use parentheses, integer, and normal division with g, h and i to get 
+#A) 1
+#B) 0
+#C) 8.333
+
+g = 25
+h = 9
+i = 3
+
+#A =
+
+#print("The answer to A is :",A)
+
+#B = 
+
+#print("The answer to B is :",B)
+
+#C =
+
+#print("The answer to C is :",C)
+
+
+ 
